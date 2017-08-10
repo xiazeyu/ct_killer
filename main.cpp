32acb6b95b10dad7af657db1e431a65852b712bf
xiazeyu_2011 <xiazeyu_2011@126.com>
2017-08-10 21:03:55 +0800

    Update the program v 1.0

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+
+	Control Killer v1.0 By Xia
+
+*/
+
+
+// #include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	
+    system("start ntsd -c -q -pn ""StudentMain.exe""");
+    return 0;
+
+}
